1563f7598f0a5786a6615c2e4ce188069f6374a8
Aditi Sharma <46081030+AditiSharma100@users.noreply.github.com>
2019-05-21 16:46:03 -0400

    Add files via upload

--- src/app/services/apiservice.ts
+++ src/app/services/apiservice.ts
@@ -1,1 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Observable, ReplaySubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+@Injectable()
+export class ApiService {
+    private chartdatasource = new ReplaySubject<any>();
+    chartdata = this.chartdatasource.asObservable();
+    constructor(private httpClient: HttpClient) {
 
+    }
+    getProductList(): Observable<any> {
+        return this.httpClient
+            .get("http://jayglovesdc.com:8080/bny/complaints/allProd");
+
+    }
+
+    getCompanyList(): Observable<any> {
+        return this.httpClient
+            .get("http://jayglovesdc.com:8080/bny/complaints/allComp");
+
+    }
+
+    getComplaintList(company,product): Observable<any> {
+        return this.httpClient
+            .get("http://jayglovesdc.com:8080/bny/complaints/combo/"+product+"/"+company);
+
+    }
+
+    updateChartData(data: any) {
+        this.chartdatasource.next(data);
+    }
+}
